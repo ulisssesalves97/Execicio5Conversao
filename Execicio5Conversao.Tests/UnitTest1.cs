218a9ddc7361e775535f8a5ba5ee3473f12d5bea
Tiago Raposo <tiago.o.raposo@gmail.com>
2020-09-22 22:10:42 -0300

    Testes

---
+++ Execicio5Conversao.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Exercicio5Conversao;
+using System;
+using Xunit;
+
+namespace Execicio5Conversao.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ValidCentToKelvin()
+        {
+            Assert.Equal(453f, ConvertTemperature.CentToKelvin(180f));
+        }
+        [Fact]
+        public void ValidKelvinToCent()
+        {
+            Assert.Equal(180f, ConvertTemperature.CentToKelvin(453f));
+        }
+    }
+}
---
+++ Exercicio5Conversao/ConvertTemperature.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Exercicio5Conversao
+{
+	public static class ConvertTemperature
+	{
+		public static float KelvinToCent(float value)
+		{
+			return value - 273f;
+		}
+
+		public static float CentToKelvin(float value)
+		{
+			return value + 273f;
+		}
+	}
+}
